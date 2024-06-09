92de846d5b7b556e10950b31824205e9c7886a02
Generative-AI-Developer <163644637+Generative-AI-Developer@users.noreply.github.com>
2024-06-09 17:30:53 +0500

    upto challange5

---
+++ Module1/chalangeProject5/index.js
@@ -0,0 +1,34 @@
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+
+
+const navbar = (
+    <nav>
+        <h1>Bob's Bestro</h1>
+    <ul>
+        <li>Menu</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
